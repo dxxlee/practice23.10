6de5d5ebef2bb2a7736265d59afab2ff66d0539a
Daniel Li <149508426+dxxlee@users.noreply.github.com>
2024-10-23 15:29:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ window.onload = function () {
     const paragraph = document.getElementById('text');
     let text = paragraph.innerHTML;
 
-    // Task 1: Highlight words over 8 characters long
+    // Task 1
     const words = text.split(' ');
     const highlightedWords = words.map(word => {
         return word.length > 8 ? `<span class="highlight">${word}</span>` : word;
     });
     paragraph.innerHTML = highlightedWords.join(' ');
 
-    // Task 2: Add a link to the source
+    // Task 2
     const link = document.createElement('a');
     link.href = "https://www.inc.com/jeff-haden/this-new-linkedin-study-reveals-top-8-job-interview-questions-and-how-great-job-candidates-answer-them.html";
     link.innerText = "Source of the text";
     document.body.appendChild(link);
 
-    // Task 3: Split each sentence onto a new line
+    // Task 3
     text = paragraph.innerHTML;
     paragraph.innerHTML = text.replace(/\.\s*/g, '.<br><br>');
 
-    // Task 4: Count the number of words
+    // Task 4
     const wordCount = words.length;
     const wordCountDiv = document.getElementById('word-count');
     wordCountDiv.innerHTML = `Word count: ${wordCount}`;
